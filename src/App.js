d863c84e67d9fc059cb0c6b2f8f882c0add60241
dan <danya.belyaev@gmail.com>
2023-10-05 03:12:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/app").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:5000')
+  //       setData(response.data)
+  //     } catch (error) {
+  //       console.error('Error', error)
+  //     }
+  //   };
+
+  //   fetchData()
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className='upper'>
+        <p>Lorum ipsum что-то там хз что</p>
+      </div>
+      
+      <div className='field'>
+        {(typeof data.members === 'undefined') ? (
+          <p>Loading...</p>
+        ): (
+        data.members.map((members, i) => (
+          <p key={i}>{members}</p>
+          ))
+        )}
+      </div>
+
     </div>
   );
 }
