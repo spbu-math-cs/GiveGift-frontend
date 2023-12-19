e96320604fb2de57068e6b9a47ced6fe52a781f3
AmEl <amirelkanov0504@gmail.com>
2023-12-20 01:15:44 +0300

    Some cosmetic fixes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import './index.css';
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-import {
-    AppContext,
-    AuthContextProvider,
-    FriendContextProvider,
-    IdeasContextProvider,
-    InterestContextProvider,
-    UserContextProvider
-} from "./context";
+import {AppContext} from "./context";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
